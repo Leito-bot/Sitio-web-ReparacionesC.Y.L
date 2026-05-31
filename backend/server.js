a07860c88824a3bfa90b32797c8eb471616fdbbb
Leonel Rosso <programacion.leo2023@gmail.com>
2026-05-31 19:56:40 -0300

    Correccion

--- backend/server.js
+++ backend/server.js
@@ -86,7 +86,7 @@ app.use(express.json());
 
 app.use('/api', contactoRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Servidor funcionando');
